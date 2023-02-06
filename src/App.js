fd97677ba5f181d189cbcc9505904251e096387a
Andrey Babichev <klopipa@gmail.com>
2023-02-06 20:37:40 +0300

    feat: add local server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import About from "./About";
 
@@ -9,6 +9,22 @@ import Home from "./Home";
 import Main from "./Main";
 
 function App() {
+
+  const [users,setUsers] = useState([]);
+  useEffect(()=>{
+    fetchData();
+  },[])
+
+  const fetchData = async () => {
+    await fetch('http://localhost:3001/cities')
+    .then((res) => res.json())
+    .then((data) => setUsers(data))
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
+  console.log(users);
   return (
     <>
       <BNavbar />
